850216d695eb0d009d299b036196a053ecd1b991
Michail Shatalow <misha.shatalow@gmail.com>
2020-11-06 11:56:24 +0200

    add README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+# Golang gRPC monorepo example
+
+
+
+#### Пример генерации Golang кода из proto файлов:
+1) `cd shippy-service-consignment`
+2) `protoc ../proto/consignment/*.proto --gofast_out=plugins=grpc:proto/consignment --proto_path=../proto/consignment/`
+
+#### or
+1) `cd cd shippy-cli-consignment`
+2) `protoc ../proto/consignment/*.proto --gofast_out=plugins=grpc:proto/consignment --proto_path=../proto/consignment/`
+
+### Разбор генерации protobuf -> golang
+
++ `protoc` - компилятор protobuf (`brew install protobuf`)
++ `../proto/consignment/*.proto` - путь до proto файла/файлов
++ `--gofast_out=plugins=grpc:proto/consignment` - компилятор proto -> golang (кастомный - https://github.com/gogo/protobuf) + gRPC плагин + выходная директория
++ `--proto_path=../proto/consignment/` - путь к директории с proto файлами (издержки производства)
+
+### Линтер:
+1) `cd shippy-service-consignment`
+2) `golangci-lint run`
--- shippy-service-consignment/internal/service/service.go
+++ shippy-service-consignment/internal/service/service.go
@@ -6,20 +6,22 @@ import (
 	pb "shippy-service-consignment/proto/consignment"
 )
 
-type repo interface {
+// Необходимый интерфейс для сервисного слоя.
+type Repository interface {
 	Create(consignment *pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
 type Service struct {
-	repo repo
+	repo Repository
 }
 
-func NewService(r repo) Service {
+// Сервис, реализующий gRPC интерфейс.
+func NewService(r Repository) Service {
 	return Service{repo: r}
 }
 
-func (s *Service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+func (s *Service) CreateConsignment(_ context.Context, req *pb.Consignment) (*pb.Response, error) {
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return nil, err
@@ -28,7 +30,7 @@ func (s *Service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
 
-func (s *Service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+func (s *Service) GetConsignments(_ context.Context, _ *pb.GetRequest) (*pb.Response, error) {
 	consignments := s.repo.GetAll()
 	return &pb.Response{Consignments: consignments}, nil
 }
